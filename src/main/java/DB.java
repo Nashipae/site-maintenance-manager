807c4e36f2e6b6f3b3cfd00d499b15c9bcf2ea39
[none] <[nwariara@safaricom.co.ke]>
2019-06-04 16:41:39 +0100

    edited the DB class.

--- src/main/java/DB.java
+++ src/main/java/DB.java
@@ -10,30 +10,31 @@ import java.net.URISyntaxException;
 
 
 public class DB {
-    public static Sql2o sql2o= new Sql2o("jdbc:postgresql://localhost:5432/site_manager", "postgres", "postgres");
+   // public static Sql2o sql2o= new Sql2o("jdbc:postgresql://localhost:5432/site_manager", "postgres", "postgres");
+    public static Sql2o sql2o= new Sql2o(Constant.URL_PROD, Constant.USERNAME, Constant.PASSWORD);
+
+    public static URI dbUri;
+    //public static Sql2o sql2o;
+    static Logger logger = LoggerFactory.getLogger(DB.class);
+
+    static {
+        try{
+            if(System.getenv("DATABASE_URL")==null){
+//                dbUri = new URI(Constants.URL_PROD);
+                dbUri = new URI("postgres://localhost:5432/site_manager");
+                logger.info("Using local database.");
+            } else {
+                dbUri = new URI(System.getenv("DATABASE_URL"));
+            }
+
+            int port = dbUri.getPort();
+            String host = dbUri.getHost();
+            String path = dbUri.getPath();
+            String username = (dbUri.getUserInfo()==null) ? Constant.USERNAME : dbUri.getUserInfo().split(":")[0];
+            String password = (dbUri.getUserInfo()==null) ? Constant.PASSWORD : dbUri.getUserInfo().split(":")[1];
+            sql2o = new Sql2o("jdbc:postgresql://"+host+":"+ port + path, username, password);
+        }catch (URISyntaxException e){
+            logger.error("Database connection failed.");
+        }
+    }
 }
-//public static URI dbUri;
-//    public static Sql2o sql2o;
-//    static Logger logger = LoggerFactory.getLogger(DB.class);
-//
-//    static {
-//        try{
-//            if(System.getenv("DATABASE_URL")==null){
-////                dbUri = new URI(Constants.URL_PROD);
-//                dbUri = new URI("postgres://localhost:5432/site_manager");
-//                logger.info("Using local database.");
-//            } else {
-//                dbUri = new URI(System.getenv("DATABASE_URL"));
-//            }
-//
-//            int port = dbUri.getPort();
-//            String host = dbUri.getHost();
-//            String path = dbUri.getPath();
-//            String username = (dbUri.getUserInfo()==null) ? Constants.USERNAME : dbUri.getUserInfo().split(":")[0];
-//            String password = (dbUri.getUserInfo()==null) ? Constants.PASSWORD : dbUri.getUserInfo().split(":")[1];
-//            sql2o = new Sql2o("jdbc:postgresql://"+host+":"+ port + path, username, password);
-//        }catch (URISyntaxException e){
-//            logger.error("Database connection failed.");
-//        }
-//    }
-//}
